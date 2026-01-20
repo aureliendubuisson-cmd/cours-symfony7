3bd6280a2962ff9a39a2e8a8bfd4903cb69cd6c2
aurelien <aurelien.dubuisson@sciencespo.fr>
2026-01-20 16:27:39 +0100

    feat: add a new controller

---
+++ src/Controller/MainController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controller;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+class MainController
+{
+    #[Route('/')]
+    public function homepage()
+    {
+        return new Response('<strong>Starshop</strong>: your monopoly-busting option for Starship parts!');
+    }
+}
