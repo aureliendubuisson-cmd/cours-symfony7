370ebc18a5675496c6c8021ee94abb86195c77d5
aurelien <aurelien.dubuisson@sciencespo.fr>
2026-01-27 11:32:45 +0100

    feat:enum

--- src/Repository/StarshipRepository.php
+++ src/Repository/StarshipRepository.php
@@ -1,47 +1,54 @@
 <?php
+
 namespace App\Repository;
+
 use App\Model\Starship;
+use App\Model\StarshipStatusEnum;
 use Psr\Log\LoggerInterface;
+
 class StarshipRepository
 {
     public function __construct(private LoggerInterface $logger)
     {
     }
+
     public function findAll(): array
     {
         $this->logger->info('Starship collection retrieved');
+
         return [
             new Starship(
                 1,
                 'USS LeafyCruiser (NCC-0001)',
                 'Garden',
                 'Jean-Luc Pickles',
-                'taken over by Q'
+                StarshipStatusEnum::IN_PROGRESS
             ),
             new Starship(
                 2,
                 'USS Espresso (NCC-1234-C)',
                 'Latte',
                 'James T. Quick!',
-                'repaired',
+                StarshipStatusEnum::COMPLETED
             ),
             new Starship(
                 3,
                 'USS Wanderlust (NCC-2024-W)',
                 'Delta Tourist',
                 'Kathryn Journeyway',
-                'under construction',
+                StarshipStatusEnum::WAITING
             ),
         ];
     }
 
     public function find(int $id): ?Starship
     {
-        foreach ($this->findAll() as $starship){
-            if ($starship->getId() === $id){
+        foreach ($this->findAll() as $starship) {
+            if ($starship->getId() === $id) {
                 return $starship;
             }
         }
+
         return null;
     }
 }
